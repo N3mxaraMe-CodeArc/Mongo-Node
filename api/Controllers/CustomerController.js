d9668cf606d3d068efb7a5f01e741a486ad6743c
N3mxara <nimsara.20223294@iit.ac.lk>
2023-11-17 19:18:40 +0530

    6

--- api/Controllers/CustomerController.js
+++ api/Controllers/CustomerController.js
@@ -26,13 +26,38 @@ const saveCustomer = (req,resp) =>{
    })
 }
 const findCustomer = (req,resp) =>{
-
+   Customer.findOne({nic:req.headers.nic})
+   .then(result,()=>{
+      if(result==null){
+         resp.status(404).json({status:false, message:'Customer not found'})
+      }else{
+         resp.status(200).json({status:true, data:result})
+      }
+   }).catch(error,()=>{
+      resp.status(500).json(error)
+   })
 }
 const updateCustomer = (req,resp) =>{
+   Customer.updateOne({nic:req.headers.nic},{
+      $set:{
+         name:req.body.name,
+         address:req.body.address,
+         salary:req.body.salary
+         }
+   })
+   .then(result,()=>{
+      if(result.nModified>0){
+         resp.status(201).json({status:true, message:'Customer was Updated'})
+      }else{
+         resp.status(200).json({status:false, message:'try again'})
+      }
+   }).catch(error,()=>{
+      resp.status(500).json(error)
+   })
 
 }
 const deleteCustomer = (req,resp) =>{
-
+   
 }
 const findAllCustomer = (req,resp) =>{
 
--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+
 const port = process.env.SERVER_PORT || 3000; 
 
 const app = express();
