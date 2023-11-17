fe4fd7c0bbaab50302e59a536571730baca24f90
N3mxara <nimsara.20223294@iit.ac.lk>
2023-11-17 13:44:08 +0530

    3

--- api/index.js
+++ api/index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
+require('dotenv').config();
+
+const port = process.env.SERVER_PORT || 3000; 
 
 const app = express();
 
 mongoose.connect('mongodb://localhost:27017/customer_crud')
-.then(() =>{
-   app.listen(3000,()=>{
-      console.log('Server is running on port 3000');
-   });
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  });
+
+app.use('/', (req, resp, next) => {
+  resp.send('<H1> Hello World </H1>');
 });
