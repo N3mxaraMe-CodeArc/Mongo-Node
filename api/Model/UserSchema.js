19a2dafdb6c17a671f8da80cd37f0869f6e0bc58
N3mxara <nimsara.20223294@iit.ac.lk>
2023-11-18 20:46:25 +0530

    12

--- api/Model/UserSchema.js
+++ api/Model/UserSchema.js
@@ -4,6 +4,6 @@ const UserSchema = new mongoose.Schema({
    username:{type:Number,required:true},
    fullname:{type:String,required:true},
    password:{type:String,required:true}
-})
+});
 
 module.exports = mongoose.model('Customer',UserSchema);
