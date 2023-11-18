65efe6c1b1a753d4b943e44702d163b055ad04ef
N3mxara <nimsara.20223294@iit.ac.lk>
2023-11-18 22:11:53 +0530

    13

--- api/Controllers/UserController.js
+++ api/Controllers/UserController.js
@@ -1,21 +1,30 @@
 const UserSchema = require('../Model/UserSchema');
+const bcrypt = require('bcryptjs');
 
 const signup = async (req,resp) =>{
    UserSchema.findOne({username:req.body.email}).then(result=>{
       if(result==null){
-         const temUser = new UserSchema({
-            username:req.body.email,
-            fullname:req.body.fullname,
-            password:req.body.password
-         })
-         temUser.save().then(result=>{
-            resp.status(201).json({
-               status:true,
-               message:'User created',
-               result:result
+         bcrypt.hash(req.body.password,10, function(err,hash){
+            if(err){
+               return resp.status(500).json({
+                  message:'Something went wrong'
+               })
+            }
+            const user = new UserSchema({
+               username:req.body.username,
+               fullname:req.body.fullname,
+               password:hash
+            });
+            user.save().then(saveData=>{
+               resp.status(201).json({
+                  status:true,
+                  message:'User created',
+                  result:saveData
+               }).
+               catch(error=>{
+                  resp.status(500).json(error)
+               })
             })
-         }).catch(error=>{
-            resp.status(500).json(error)
          })
       }else{
          resp.status(409).json({
