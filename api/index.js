9b9219cea68dca130bdf3cb9c5d632e0bd2f5520
N3mxara <nimsara.20223294@iit.ac.lk>
2023-11-18 19:34:15 +0530

    9

--- api/index.js
+++ api/index.js
@@ -2,18 +2,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 const bodyParser = require('body-parser');
-const cors = require('cors');
-
 const port = process.env.SERVER_PORT || 3000;
+const app = express();
 
-app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
 const CustomerRoute = require('./routes/CustomerRoute')
 
-const app = express();
 
 mongoose.connect('mongodb://localhost:27017/customer_crud')
   .then(() => {
@@ -22,4 +19,4 @@ mongoose.connect('mongodb://localhost:27017/customer_crud')
     });
   });
 
-app.use('./api/v1/customers', CustomerRoute);
+app.use(`./api/v1/customers`, CustomerRoute);
