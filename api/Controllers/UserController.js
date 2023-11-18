c3cf49cec73d4c1b687e86d90836713f72506557
N3mxara <nimsara.20223294@iit.ac.lk>
2023-11-18 23:14:53 +0530

    16

--- api/Controllers/UserController.js
+++ api/Controllers/UserController.js
@@ -1,70 +1,80 @@
 const UserSchema = require('../Model/UserSchema');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-const signup = async (req,resp) =>{
-   UserSchema.findOne({username:req.body.email}).then(result=>{
-      if(result==null){
-         bcrypt.hash(req.body.password,10, function(err,hash){
-            if(err){
+const signup = async (req, resp) => {
+   try {
+      const result = await UserSchema.findOne({ username: req.body.email });
+      if (result == null) {
+         bcrypt.hash(req.body.password, 10, async (err, hash) => {
+            if (err) {
                return resp.status(500).json({
-                  message:'Something went wrong'
-               })
+                  message: 'Something went wrong',
+               });
             }
             const user = new UserSchema({
-               username:req.body.username,
-               fullname:req.body.fullname,
-               password:hash
+               username: req.body.username,
+               fullname: req.body.fullname,
+               password: hash,
             });
-            user.save().then(saveData=>{
+            try {
+               const saveData = await user.save();
                resp.status(201).json({
-                  status:true,
-                  message:'User created',
-                  result:saveData
-               }).
-               catch(error=>{
-                  resp.status(500).json(error)
-               })
-            })
-         })
-      }else{
+                  status: true,
+                  message: 'User created',
+                  result: saveData,
+               });
+            } catch (error) {
+               resp.status(500).json(error);
+            }
+         });
+      } else {
          resp.status(409).json({
-            status:false,
-            message:'User already exist'
-         })
+            status: false,
+            message: 'User already exists',
+         });
       }
-   }).catch(error=>{
-      resp.status(500).json(error)
-   })
-}
+   } catch (error) {
+      resp.status(500).json(error);
+   }
+};
 
-const login = async (req,resp) =>{
-   UserSchema.findOne(req.params.username).then(result=>{
-      if(result==null){
+const login = async (req, resp) => {
+   try {
+      const selectedUser = await UserSchema.findOne({ username: req.body.username });
+      if (selectedUser == null) {
          resp.status(404).json({
-            status:false,
-            message:'User not found'
-         })
-      }else{
-         bcrypt.compare(req.body.password,result.password,function(err,hash){
-            if(err){
+            status: false,
+            message: 'User not found',
+         });
+      } else {
+         bcrypt.compare(req.body.password, selectedUser.password, (err, result) => {
+            if (err) {
                return resp.status(401).json({
-                  message:'Auth failed'
-               })
+                  message: 'Auth failed',
+               });
             }
-            if(result){
+            if (result) {
+               const expiresIn = 3600;
+               const token = jwt.sign(
+                  { username: selectedUser.username },
+                  process.env.SECRET_KEY,
+                  { expiresIn: expiresIn }
+               );
+               resp.setHeader('Authorization', `Bearer ${token}`);
                return resp.status(200).json({
-                  token:'token data'
-               })
-            }else{
+                  token: token,
+               });
+            } else {
                return resp.status(401).json({
-                  message:'Auth failed'
-               })
+                  message: 'Auth failed',
+               });
             }
-         })
+         });
       }
-   }).catch(error=>{
-      resp.status(500).json(error)
-   })
-}
+   } catch (error) {
+      resp.status(500).json(error);
+   }
+};
 
-module.exports = {signup,login}
+module.exports = { signup, login };
